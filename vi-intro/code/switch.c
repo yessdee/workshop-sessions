359347f754e25c92dc19705bf5a4005d1b6b9912
Deepak <deepak@zilogic.com>
2018-10-14 23:33:04 +0530

    Adding intial contents of vi session

---
+++ vi-intro/code/switch.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	switch (10 * 5 + 2) {
+		// int i = 10;
+		case 52:
+			i = 20;
+			printf("%d\n", i);
+		case 53:
+			i++;
+			break;
+		default:
+			printf("%d\n", i);
+
+	}
+}
